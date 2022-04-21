b07d735bd2107247c05647ff47a647e9b550ca28
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-04-21 16:32:46 -0300

    Requisito 3

--- src/hooks/components/Table.js
+++ src/hooks/components/Table.js
@@ -4,20 +4,83 @@ import APIContext from '../../context/APIContext';
 function Table() {
   const { data } = useContext(APIContext);
   const [filterByName, setFilterByName] = useState('');
+  const [column, setColumn] = useState('population');
+  const [comparison, setComparison] = useState('maior que');
+  const [value, setValue] = useState(0);
+  const [filterByNumericValues, setFilterByNumericValues] = useState([]);
 
   const planetName = data.filter((values) => values.name.includes(filterByName));
 
+  const filtering = planetName.filter((item) => {
+    if (filterByNumericValues.comparison === 'menor que') {
+      return item[filterByNumericValues.column] < Number(filterByNumericValues.value);
+    }
+    if (filterByNumericValues.comparison === 'maior que') {
+      return item[filterByNumericValues.column] > Number(filterByNumericValues.value);
+    }
+    if (filterByNumericValues.comparison === 'igual a') {
+      return Number(item[filterByNumericValues.column]) === Number(filterByNumericValues
+        .value);
+    }
+    return item;
+  });
+
+  const handleClick = () => {
+    setFilterByNumericValues({ column, comparison, value });
+  };
+
   return (
     <section>
-      <input
-        data-testid="name-filter"
-        type="text"
-        name="name"
-        placeholder="Nome do planeta"
-        value={ filterByName }
-        onChange={ (e) => setFilterByName(e.target.value) }
-      />
       <h1>Star Wars</h1>
+      <div>
+        <input
+          data-testid="name-filter"
+          type="text"
+          name="name"
+          placeholder="Nome do planeta"
+          value={ filterByName }
+          onChange={ (e) => setFilterByName(e.target.value) }
+        />
+        <label htmlFor="column">
+          Coluna:
+          <select
+            data-testid="column-filter"
+            id="column"
+            onChange={ (e) => setColumn(e.target.value) }
+          >
+            <option value="population">population</option>
+            <option value="orbital_period">orbital_period</option>
+            <option value="diameter">diameter</option>
+            <option value="rotation_period">rotation_period</option>
+            <option value="surface_water">surface_water</option>
+          </select>
+        </label>
+        <label htmlFor="valor">
+          Valor:
+          <select
+            data-testid="comparison-filter"
+            id="valor"
+            onChange={ (e) => setComparison(e.target.value) }
+          >
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+        </label>
+        <input
+          data-testid="value-filter"
+          type="number"
+          defaultValue="0"
+          onChange={ (e) => setValue(e.target.value) }
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ handleClick }
+        >
+          Filtrar
+        </button>
+      </div>
       <table>
         <thead>
           <tr>
@@ -37,24 +100,24 @@ function Table() {
           </tr>
         </thead>
         {
-          planetName
+          filtering
             ? (
-              planetName.map((value) => (
-                <tbody key={ value.name }>
+              filtering.map((item) => (
+                <tbody key={ item.name }>
                   <tr>
-                    <td>{ value.name }</td>
-                    <td>{ value.rotation_period }</td>
-                    <td>{ value.orbital_period }</td>
-                    <td>{ value.diameter }</td>
-                    <td>{ value.climate }</td>
-                    <td>{ value.gravity }</td>
-                    <td>{ value.terrain }</td>
-                    <td>{ value.surface_water }</td>
-                    <td>{ value.population }</td>
-                    <td>{ value.films }</td>
-                    <td>{ value.created }</td>
-                    <td>{ value.edited }</td>
-                    <td>{ value.url }</td>
+                    <td>{ item.name }</td>
+                    <td>{ item.rotation_period }</td>
+                    <td>{ item.orbital_period }</td>
+                    <td>{ item.diameter }</td>
+                    <td>{ item.climate }</td>
+                    <td>{ item.gravity }</td>
+                    <td>{ item.terrain }</td>
+                    <td>{ item.surface_water }</td>
+                    <td>{ item.population }</td>
+                    <td>{ item.films }</td>
+                    <td>{ item.created }</td>
+                    <td>{ item.edited }</td>
+                    <td>{ item.url }</td>
                   </tr>
                 </tbody>
               )))
